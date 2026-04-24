8fdc8f098384764460212c8edd8d1de4ac58ccca
Kiefer <kiefhay642@outlook.com>
2026-04-24 15:16:31 -0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.rs
@@ -0,0 +1,32 @@
+impl Solution {
+    pub fn encode(strs: Vec<String>) -> String {
+        let mut result = String::new();
+        for s in &strs {
+            result.push_str(&s.len().to_string());
+            result.push('#');
+            result.push_str(s);
+        }
+
+        result
+    }
+
+    pub fn decode(s: String) -> Vec<String> {
+        let mut result = Vec::new();
+        let bytes = s.as_bytes();
+        let mut i = 0;
+
+        while i < bytes.len() {
+            let mut j = s[i..].find('#').unwrap() + i;
+            let len: usize = s[i..j]
+                .parse()
+                .unwrap_or_else(|_| panic!("Failed to parse length from '{}'", &s[i..j]));
+
+            i = j + 1;
+            j = i + len;
+            result.push(s[i..j].to_string());
+            i = j;
+        }
+
+        result
+    }
+}
